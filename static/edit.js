1cf0a918315ef60899ddc2c26ef5a9a5d8c0afff
pɹolʇɐƆ <134858054+element10101@users.noreply.github.com>
2023-08-23 09:49:16 -0500

    Documents

--- static/edit.js
+++ static/edit.js
@@ -1,1 +1,19 @@
+class _Document { // The underscore is required because `Document` is reserved by JS
+  constructor(title) {
+    this.title = title;
+    this.content = "";
+  }
+  editTitle(title) {
+    this.title = title;
+  }
+  editContent(content) {
+    this.content = content;
+  }
+}
 
+let doc = new _Document("Untitled document");
+
+function save() {
+  doc.editTitle(document.getElementById("title").value);
+  doc.editContent(document.getElementById("content").value);
+}
