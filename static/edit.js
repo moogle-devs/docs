ac8457cbdc0afc75d928d3267606280252204a9b
pɹolʇɐƆ <134858054+element10101@users.noreply.github.com>
2023-08-29 10:16:50 -0500

    Word count & Edit/view mode

--- static/edit.js
+++ static/edit.js
@@ -15,11 +15,11 @@ class _Document { // The underscore is required because `Document` is reserved b
   }
 }
 
-let doc = new _Document("Untitled document");
+let doc = new _Document("Untitled Document");
 
 function save() {
   doc.editTitle($("#title").value);
-  doc.editContent($("#content").value);
+  doc.editContent($("#content").innerHTML);
 }
 
 // Formatting functions
@@ -40,5 +40,32 @@ function underline() {
 
 function switchColor() {
   let s = window.getSelection().toString();
-  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="color: ${$("#tcolor").value}">${s}</span>`)
+  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="color: ${$("#tcolor").value}">${s}</span>`);
 }
+
+function switchFont() {
+  let s = window.getSelection().toString();
+  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="font-family: ${$("#font").value}">${s}</span>`);
+}
+
+function switchFontSize() {
+  let s = window.getSelection().toString();
+  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="font-size: ${parseInt($("#fsize").value)}px;">${s}</span>`)
+}
+
+// Other functions
+function toggleMode(params) {
+  if($("#togglemode").value == "edit") {
+    $("#content").setAttribute("contenteditable", "true");
+  }
+  else {
+    $("#content").removeAttribute("contenteditable");
+  }
+}
+
+// Keyboard shortcuts
+document.onkeyup = (e) => {
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
+    alert(`Word count:\nWords: ${doc.content.split(" ").length}\nCharacters: ${doc.content.length}`);
+  }
+};
