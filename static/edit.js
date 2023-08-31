172bc9eb1fdd807e435b9a6b883856caabcbeffc
pɹolʇɐƆ <134858054+element10101@users.noreply.github.com>
2023-08-31 10:05:37 -0500

    Strikethroughs and more formatting options

--- static/edit.js
+++ static/edit.js
@@ -6,12 +6,16 @@ class _Document { // The underscore is required because `Document` is reserved b
   constructor(title) {
     this.title = title;
     this.content = "";
+    this.variables = {};
   }
   editTitle(title) {
     this.title = title;
   }
   editContent(content) {
     this.content = content;
+  }
+  editVariable(name, value) {
+    this.variables[name] = value;
   }
 }
 
@@ -38,6 +42,11 @@ function underline() {
   $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span class="underline">${s}</span>`);
 }
 
+function strikethrough() {
+  let s = window.getSelection().toString();
+  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span class="strikethrough">${s}</span>`)
+}
+
 function switchColor() {
   let s = window.getSelection().toString();
   $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="color: ${$("#tcolor").value}">${s}</span>`);
@@ -53,8 +62,35 @@ function switchFontSize() {
   $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="font-size: ${parseInt($("#fsize").value)}px;">${s}</span>`)
 }
 
+function switchBackgroundColor() {
+  let s = window.getSelection().toString();
+  $("#content").innerHTML = $("#content").innerHTML.replace(s, `<span style="background-color: ${$('#bcolor').value};">${s}</span>`);
+}
+
+// Option dropdowns
+function execToolsBar() {
+  switch($("#tools-bar").value) {
+    case "word-count":
+      wordCount();
+      break;
+    case "variables":
+      variables();
+      break;
+  }
+  $("#tools-bar").value = "tools";
+}
+
+function execFormatBar() {
+  switch($("#format-bar").value) {
+    case "text":
+      textOptions();
+      break;
+  }
+  $("#format-bar").value = "format";
+}
+
 // Other functions
-function toggleMode(params) {
+function toggleMode() {
   if($("#togglemode").value == "edit") {
     $("#content").setAttribute("contenteditable", "true");
   }
@@ -63,9 +99,64 @@ function toggleMode(params) {
   }
 }
 
-// Keyboard shortcuts
-document.onkeyup = (e) => {
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
-    alert(`Word count:\nWords: ${doc.content.split(" ").length}\nCharacters: ${doc.content.length}`);
+function wordCount() {
+  alert(`Word count:\nWords: ${doc.content.split(" ").length}\nCharacters: ${doc.content.length}`);
+}
+
+function variables() {
+  let varDialog = prompt("Variables\nType 'create' to create a variable");
+  if (varDialog.toLowerCase() === "create") {
+    let varName = prompt("Variable Name:");
+    if (varName != null) {
+      let varValue = prompt("Variable Value:");
+      if(varValue != null) {
+        doc.editVariable(varName, varValue);
+      }
+    }
   }
-};
+}
+
+function showHideMenu() {
+  if($("#menu").hasAttribute("hidden")) {
+    $("#menu").removeAttribute("hidden");
+  }
+  else {
+    $("#menu").setAttribute("hidden", "true");
+  }
+}
+
+function textOptions() {
+  let TODialog = prompt("Type `bold`, `italic`, `underline`, or `strikethrough`:");
+  if(TODialog != null) {
+    switch(TODialog) {
+      case "bold":
+        bold();
+        break;
+      case "italic":
+        italic();
+        break;
+      case "underline":
+        underline();
+        break;
+      case "strikethrough":
+        strikethrough();
+        break;
+      default:
+        alert("You did not enter a valid option.");
+        break;
+    }
+  }
+}
+
+// Keyboard shortcuts
+document.addEventListener('keyup', (e) => {
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 191) {
+    alert("Keyboard shortcuts:\nWord count: [Ctrl][Shift][C]")
+  } else if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
+    wordCount();
+  } else if (e.ctrlKey && e.shiftKey && e.keyCode == 70) {
+    showHideMenu();
+  } else if (e.altKey && e.shiftKey && (e.keyCode == 53 || e.keyCode == 101)) {
+    strikethrough();
+  }
+});
